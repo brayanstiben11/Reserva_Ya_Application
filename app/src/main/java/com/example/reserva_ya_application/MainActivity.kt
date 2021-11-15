1a837679356a47f9ba204750cf870ba17bf42927
Brayan <bvelasquez01@uan.edu.co>
2021-11-14 23:48:52 -0500

    Update Login

--- app/src/main/java/com/example/reserva_ya_application/MainActivity.kt
+++ app/src/main/java/com/example/reserva_ya_application/MainActivity.kt
@@ -3,38 +3,40 @@ package com.example.reserva_ya_application
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val email : EditText = findViewById(R.id.email1)
-        val password : EditText = findViewById(R.id.pass)
-        val iniciar : Button = findViewById(R.id.login)
-        val registrar : Button = findViewById(R.id.registrar)
+        auth = Firebase.auth
 
+        val email: EditText = findViewById(R.id.email1)
+        val password: EditText = findViewById(R.id.pass)
+        val iniciar: Button = findViewById(R.id.login)
+        val registrar: Button = findViewById(R.id.registrar)
 
         iniciar.setOnClickListener()
         {
-            if (email.text.toString() == "brayan@gmail.com" && password.text.toString() == "12345")
-            {
-                val intent = Intent (this, Ver_Hoteles :: class.java)
-                startActivity(intent)
+            if (email.text.isNotEmpty() && password.text.isNotEmpty()) {
+                SignIn(email.text.toString(), password.text.toString())
             }
-            else if   (email.text.isEmpty() || password.text.isEmpty() )
-            {
-                Toast.makeText(this,"Los campos deben ser llenados", Toast.LENGTH_LONG).show()
-            }
+
             else
             {
-                Toast.makeText(this,"Los datos son invalidos", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "Llene completamente los espacios", Toast.LENGTH_SHORT).show()
             }
-
-
         }
 
         registrar.setOnClickListener()
@@ -42,5 +44,27 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent (this,Registrar::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun SignIn (email: String, password: String)
+    {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d("TAG", "signInWithEmail:success")
+                    Toast.makeText(this,"Has entrado a tu cuenta satisfactoriamente",Toast.LENGTH_SHORT).show()
+                    ReLoad()
+
+                } else {
+                    Log.w("TAG", "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "TUS DATOS SON INCORRECTOS",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+    private fun ReLoad()
+    {
+        val intent = Intent (this, Ver_Hoteles :: class.java )
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/reserva_ya_application/Registrar.kt
+++ app/src/main/java/com/example/reserva_ya_application/Registrar.kt
@@ -3,14 +3,21 @@ package com.example.reserva_ya_application
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class Registrar : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registrar)
+        auth = Firebase.auth
 
         val login : Button = findViewById(R.id.gologin)
         val new : Button = findViewById(R.id.signup)
@@ -27,11 +34,8 @@ class Registrar : AppCompatActivity() {
         new.setOnClickListener()
         {
             if(nombre.text.isNotEmpty() && email.text.isNotEmpty() && pass.text.isNotEmpty()) {
-                val intent = Intent(this, Perfil::class.java)
-                intent.putExtra("nombre", nombre.text.toString())
-                intent.putExtra("correo", email.text.toString())
-                intent.putExtra("password", pass.text.toString())
-                startActivity(intent)
+
+                crearusuario(email.text.toString(),pass.text.toString())
             }
             else if (nombre.text.isEmpty() || email.text.isEmpty() || pass.text.isEmpty())
             {
@@ -39,5 +43,30 @@ class Registrar : AppCompatActivity() {
             }
         }
 
+    }
+
+    private fun crearusuario (email : String, password : String)
+    {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("TAG", "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    Toast.makeText(this,"LA CUENTA FUE CREADA SATISFACTORIAMENTE",Toast.LENGTH_SHORT).show()
+                    next()
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("TAG", "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "YA EXISTE UNA CUENTA CON ESTE CORREO",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+    private fun next ()
+    {
+        val intent = Intent (this, Ver_Hoteles :: class.java )
+        startActivity(intent)
     }
 }
